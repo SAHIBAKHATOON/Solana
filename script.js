5e4153f550f6db88b5695a118d3ecc5974387e6d
SAHIBAKHATOON <115203203+SAHIBAKHATOON@users.noreply.github.com>
2024-06-22 18:12:06 +0530

    script-add

--- script.js
+++ script.js
@@ -1,40 +1,4 @@
-// document.addEventListener('DOMContentLoaded', (event) => {
-//     const toggleButton = document.getElementById('darkModeToggle');
-//     const body = document.body;
-
-//     const setButtonText = () => {
-//       if (body.classList.contains('dark-mode')) {
-//         toggleButton.textContent = 'dark';
-//       } else {
-//         toggleButton.textContent = "light" ;
-//       }
-//     };
-
-  
-//     if (localStorage.getItem('mode') === 'dark') {
-//       body.classList.add('dark-mode');
-//     } else {
-//       body.classList.add('light-mode');
-//     }
-
-    
-//     setButtonText();
-
-//     toggleButton.addEventListener('click', () => {
-//       if (body.classList.contains('light-mode')) {
-//         body.classList.remove('light-mode');
-//         body.classList.add('dark-mode');
-//         localStorage.setItem('mode', 'dark');
-//       } else {
-//         body.classList.remove('dark-mode');
-//         body.classList.add('light-mode');
-//         localStorage.setItem('mode', 'light');
-//       }
-//       setButtonText();
-//     });
-//   });
-
-   const setTheme = theme => document.documentElement.className = theme;
+const setTheme = theme => document.documentElement.className = theme;
 
 
  
